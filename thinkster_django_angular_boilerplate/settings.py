01a771b0e3f835b98724354c7c32cefef5ab7361
David Eder <david.eder26@gmail.com>
2017-01-23 21:59:00 -0600

    added whitenoise middleware

--- thinkster_django_angular_boilerplate/settings.py
+++ thinkster_django_angular_boilerplate/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.WhiteNoiseMiddleware',
 )
 
 ROOT_URLCONF = 'thinkster_django_angular_boilerplate.urls'
