aef9a876b34c1d71ed642abf64c88a33c79d77c5
David Eder <david.eder26@gmail.com>
2017-01-23 21:33:46 -0600

    Hopefully static files work

--- thinkster_django_angular_boilerplate/settings.py
+++ thinkster_django_angular_boilerplate/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -84,8 +85,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
 STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
@@ -96,6 +97,8 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
     'compressor.finders.CompressorFinder',
 )
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 COMPRESS_ENABLED = os.environ.get('COMPRESS_ENABLED', False)
 
@@ -135,3 +138,4 @@ LOGGING = {
             }
         },
 }
+
--- thinkster_django_angular_boilerplate/wsgi.py
+++ thinkster_django_angular_boilerplate/wsgi.py
@@ -7,10 +7,17 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
+
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "thinkster_django_angular_boilerplate.settings")
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+"""
+from whitenoise.django import DjangoWhiteNoise
 
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
+"""
+
+from dj_static import Cling
 application = Cling(get_wsgi_application())
